7fe1755b91a989db77d4e91b06093e1a671f4cce
rogeriosims <rogeriosims@gmail.com>
2026-03-13 21:00:24 -0300

    feat: Introduce a new `BaseLayout` component with comprehensive SEO and analytics setup, and remove `@astrojs/partytown` integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,11 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from '@tailwindcss/vite';
-import partytown from '@astrojs/partytown';
 
 export default defineConfig({
   site: 'https://notafacildownloader.contabilcert.com.br',
   integrations: [
     sitemap(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
   ],
   vite: {
     plugins: [tailwindcss()]
